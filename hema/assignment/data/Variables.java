acbaef626acda5636aa8ca1d98b06f932a3822e3
hemasri098 <59275471+hemasri098@users.noreply.github.com>
2020-03-25 19:14:56 +0530

    added loggers

--- hema/assignment/data/Variables.java
+++ hema/assignment/data/Variables.java
@@ -1,16 +1,19 @@
-package hema.assignment.data;
+package com.company.JavaAssignment5.data;
+
+import java.util.logging.Logger;
 
 public class Variables {
     int int_variable;
     char char_variable;
+    Logger LOGGER = Logger.getAnonymousLogger();
     public void printVariables()
     {
-        System.out.println("integer: " + int_variable + "\n" + "character: " + char_variable);
+        LOGGER.info("integer: " + int_variable + "\n" + "character: " + char_variable);
     }
     public void printLocalVariables() {
         //int variable_1;
         //char variable_2;
-        //System.out.println("integer: " + variable_1 + "\n" + "character: " + variable_2); //Cannot access local variables that are not intialized
+        //LOGGER.info("integer: " + variable_1 + "\n" + "character: " + variable_2); //Cannot access local variables that are not intialized
 
     }
 }
